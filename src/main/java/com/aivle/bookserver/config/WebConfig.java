fa776c06272b9363884680130a05aeadf16a90b6
ppiyak1231 <akstj123@naver.com>
2026-06-23 17:30:55 +0900

    Ci: WebConfig.java 내 요로컬 제외 다른 요청 허용

--- src/main/java/com/aivle/bookserver/config/WebConfig.java
+++ src/main/java/com/aivle/bookserver/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "*")
                 .allowedMethods(
                         "GET",
                         "POST",
