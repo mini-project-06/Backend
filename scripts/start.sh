6404c809da31be48f570f2bfbf1308602f9732cb
mxnssuu <paul990129@gmail.com>
2026-06-24 14:47:21 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+
+cp /home/ec2-user/.env /home/ec2-user/app/.env
+
 JAR=$(ls /home/ec2-user/app/build/libs/*.jar | grep -v plain | head -n 1)
 echo "Starting: $JAR"
+cd /home/ec2-user/app
 nohup java -jar $JAR > /home/ec2-user/app/app.log 2>&1 &
